f630e6cc35b1fe1b731d8428f52f6da6c21d1e06
haiqiang1995 <362527596@qq.com>
2020-01-02 10:53:35 +0800

    登录注册提交

--- js/web-style.js
+++ js/web-style.js
@@ -1,4 +1,22 @@
 $(function () {
     console.log('aaaa')
+
+/*---------------------------------  HQ  --------------------------------*/
+
+    //TAB切换
+    $('.qtab').each(function(){
+        $(this).children('.qtab1').eq(0).addClass('on');
+    });
+
+    $('.qtab_nr').each(function(){
+        $(this).children('.qtab_nr1').eq(0).show();
+    });
+
+    $('.qtab1').click(function(){
+    // $('.qtab1').mouseover(function(){
+        $(this).addClass('on').siblings().removeClass('on');
+        $(this).parents().siblings('.qtab_nr').children('.qtab_nr1').eq($(this).index()).show().siblings().hide();
+    });
+
 })
 
